558f895ca16afb31819ea1c4f3224013228f005b
Xewar <xewardreams@gmail.com>
2022-01-11 13:16:52 -0500

    first draft of home page with tabs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
-console.log('hi love');
-console.log('how are you?')
-console.log('fine thanks')
-// alert('hola mona');
+import './styles.css';
+import Icon from './icon.png';
+
+//Creating the header and tabs
+let header = document.querySelector('#header');
+let zaad = document.createElement('h1');
+zaad.textContent = 'Zaad';
+header.appendChild(zaad);
+let buttons = ['Welcome','Menu','Contact'];
+let tabs = document.createElement('div');
+tabs.setAttribute('id','tabs');
+for (let button of buttons) {
+    let btn = document.createElement('button');
+    btn.textContent = button;
+    btn.setAttribute('class', 'navigationButton');
+    tabs.appendChild(btn);
+}
+header.appendChild(tabs);
+
+
+let content = document.querySelector('#content');
+let holder = document.createElement('div');
+const myIcon = new Image();
+myIcon.src = Icon;
+myIcon.style.width = '229px';
+holder.appendChild(myIcon)
+content.appendChild(holder);
