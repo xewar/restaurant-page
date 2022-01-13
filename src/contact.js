9ef2a8c60b6e50301fbb9c205aee40d85905b0a4
Xewar <xewardreams@gmail.com>
2022-01-12 20:42:30 -0500

    added contact tab

--- src/contact.js
+++ src/contact.js
@@ -1,10 +1,30 @@
+import falafelPic from './images/falafel.jpg'
+
 const contactTab = () => {
     let container = document.createElement('div');
-    let text_div = document.createElement('div');
-    text_div.textContent = 'Testing tab switching'
-    container.appendChild(text_div);
     container.setAttribute('class','container');
-    content.appendChild(container);
+    let text_div = document.createElement('div');
+    //could not figure out how to get the line break to work below
+    text_div.innerHTML = `Order via Seamless or Doordash,
+     or by calling us at 212-999-9999`;
+    text_div.setAttribute('class','home_text');
+    container.appendChild(text_div);
+
+    let cateringText = document.createElement('div');
+    cateringText.textContent = `We also cater events big and small. Get in touch with the 
+    details about your event and we'll do our best to accommodate your desired date 
+    and any requests you may have to make your celebration memorable.`
+    cateringText.setAttribute('class','catering');
+    container.appendChild(cateringText);
+    
+
+     //adding an image
+     const falafel = new Image();
+     falafel.src = falafelPic;
+     let image_div = document.createElement('div');
+     image_div.appendChild(falafel);
+     image_div.setAttribute('class','food_pic');
+     content.appendChild(container);
 }
 
 export {
--- src/welcome.js
+++ src/welcome.js
@@ -5,12 +5,17 @@ function welcomeTab() {
     let container = document.createElement('div');
     container.setAttribute('class','container');
     let box = document.createElement('box')
+
+    //adding an image
     const chicken = new Image();
     chicken.src = chickenR;
     let image_div = document.createElement('div');
     image_div.appendChild(chicken);
     image_div.setAttribute('class','food_pic')
+
+    //adding text
     let text_div = document.createElement('div');
+    text_div.setAttribute('class','home_text')
     text_div.textContent = `With some of the most delicious combinations of ingredients, Zaad has something for everyone to enjoy.
     Loved by locals and traveling foodies alike, now is the perfect time for you to join us!`
     container.appendChild(image_div);
